20e561d563314d81c8ba35622727b590bfd7e12d
budhaditya-itobuz <it012303@IT012303s-MacBook-Pro.local>
2024-03-12 13:01:35 +0530

    first commit

---
+++ Data/productPageData.js
@@ -0,0 +1,26 @@
+export const product={
+    img:[
+        "../Images/Product/book.png","../Images/Product/book-tilted.png","../Images/Product/book-open.png","../Images/Product/book-open.png"
+    ],
+    title:"Embracing All with Redeeming Love Book",
+    price:86.44,
+    discount:20,
+    viewing:46,
+    description:"It is a long established fact that a reader will be distracted by the readable content of a page when looking.",
+    stockLeft:102,
+    details:{
+        header:"Style No. 68755644; Color Code: 038Turn heads in this stunning maxi dress featured in a forever classic halterneck silhouettewith gorgeous.",
+        list:[
+            "Effortless, pull-on style",
+            "Tiered design",
+            "Deep V-neckline"
+        ]
+    },
+    careImport:["Dolman style sleeves","Side pockets","Deep V-neckline","Discreet bust clasp for modesty","Large decorative bow detail at front"],
+    careImport2:[
+        "Bump and breast feeding friendly",
+        "100% Crinkle Viscose",
+        "Cold Hand Wash"
+    ]
+
+}
